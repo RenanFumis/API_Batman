d4bd1cc5c82a8ffd2baa30710da7ea783d01e18a
RenanFumis <renan-oliveira19@hotmail.com>
2024-04-27 17:22:27 -0300

    criando um ambiente de teste primário das requisições

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Criado por Bob Kane e escritor Bill Finger, Batman eh um personagem de quadrinhos publicado pela DC Comics em 1939",
+  "/herois": "Lista de herois do universo Batman",
+  "/viloes": "Lista de viloes do universo Batman",
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Servidor saudavel \nBatman eh o melhor!");
+  res.end(rotas[req.url] || "Rota nao encontrada");
 });
 
 server.listen(PORT, () => {
