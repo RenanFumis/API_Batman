b60b63884241e220160d81f459bc26b46de1fc6a
RenanFumis <renan-oliveira19@hotmail.com>
2024-04-27 00:33:15 -0300

    instalação do nodemon

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Servidor rodando \nBatman eh o melhor!");
+  res.end("Servidor saudavel \nBatman eh o melhor!");
 });
 
 server.listen(PORT, () => {
