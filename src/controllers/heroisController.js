c1ff177dc9e9bc908b9c50d4d751d244cd2a46ed
RenanFumis <renan-oliveira19@hotmail.com>
2024-05-08 19:29:12 -0300

    ajustei o código para que ele trate os erros de validação, sendo assim qualquer CRUD em id não existente, vai enviar um aviso ao frontend

--- src/controllers/heroisController.js
+++ src/controllers/heroisController.js
@@ -1,4 +1,5 @@
 //Responsável por controlar as requisições feitas para a API relacionadas aos herois
+import NaoEncontrado from "../Erros/NaoEncontrado.js";
 import heroi from "../models/Herois.js";
 
 class HeroisController {
@@ -18,9 +19,7 @@ class HeroisController {
       if(heroiEncontrado !== null){
         res.status(200).json(heroiEncontrado);
       }else{
-        res
-        .status(404)
-        .send({ message: `Heroi não encontrado`});
+        next(new NaoEncontrado("Heroi não encontrado"))
       }
     } catch (erro) {
       next(erro);
@@ -41,7 +40,13 @@ class HeroisController {
   static async atualizarHeroi(req, res, next) {
     try {
       const id = req.params.id;
-      await heroi.findByIdAndUpdate(id, req.body);
+      const heroiUpdate = await heroi.findByIdAndUpdate(id, req.body);
+
+      if(heroiUpdate !== null){
+        res.status(200).json({ message: "Heroi atualizado com sucesso" });
+      }else{
+        next(new NaoEncontrado("Heroi não encontrado"));
+      }
       res.status(200).json({ message: "Heroi atualizado com sucesso" });
     } catch (erro) {
       next(erro);
@@ -51,8 +56,14 @@ class HeroisController {
   static async excluirHeroi(req, res, next) {
     try {
       const id = req.params.id;
-      await heroi.findByIdAndDelete(id);
-      res.status(200).json({ message: "Heroi excluido com sucesso" });
+      const heroiEliminado = await heroi.findByIdAndDelete(id);
+
+      if(heroiEliminado !== null){
+        res.status(200).json({ message: "Heroi excluido com sucesso" });
+      }else{
+        next(new NaoEncontrado("Heroi não encontrado"));
+      }
+      
     } catch (erro) {
       next(erro);
     }
--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import ErroBase from "../Erros/ErroBase.js";
 import ErroValidacao from "../Erros/ErroValidacao.js";
 import RequisicaoIncorreta from "../Erros/RequisicaoIncorreta.js";
+import NaoEncontrado from "../Erros/NaoEncontrado.js";
 
 function manipuladorDeErros(erro, req, res, next){
   if (erro instanceof mongoose.Error.CastError){
@@ -9,6 +10,9 @@ function manipuladorDeErros(erro, req, res, next){
   }else if(erro instanceof mongoose.Error.ValidationError){
     
     new ErroValidacao(erro).enviarResposta(res);
+  }else if(erro instanceof NaoEncontrado){
+    erro.enviarResposta(res);
+  
   }else{
     new ErroBase().enviarResposta(res);
   }
