9016fbbe854dce70812c0dc79f7a61fd54988e1d
RenanFumis <renan-oliveira19@hotmail.com>
2024-05-08 14:26:01 -0300

    criado uma patronização de aviso dos erros de falta de informação nos dados.

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 function manipuladorDeErros(erro, req, res, next){
   if (erro instanceof mongoose.Error.CastError){
     res.status(400).send({ message: "Um ou mais parâmetros são inválidos."})
+  }else if(erro instanceof mongoose.Error.ValidationError){
+    const mensagensErro = Object.values(erro.errors).map((erro) => erro.message).join(", ");
+    
+    res.status(400).send({message: `Os seguintes erros foram encontrados: ${mensagensErro}`});
   }else{
     res.status(500).send({ message: `ERROR: Erro interno de Servidor.` });
   }
--- src/models/Viloes.js
+++ src/models/Viloes.js
@@ -4,12 +4,21 @@ import mongoose from "mongoose";
 const viloesSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Schema.Types.ObjectId },
-    nome: { type: String, required: true },
-    alter_ego: { type: String, required: true },
+    nome: {
+      type: String,
+      required: [true, "O campo NOME é obrigatório."]
+    },
+    alter_ego: {
+      type: String,
+      required: [true, "O campo ALTER EGO é obrigatório."]
+    },
     profissao: { type: String },
     genero: { type: String },
     estado_civil: { type: String },
-    primeira_aparicao: { type: String, required: true },
+    primeira_aparicao: {
+      type: String,
+      required: [true, "O campo PRIMEIRA APARIÇÃO é obrigatório."]
+    },
   },
   {
     versionKey: false,
