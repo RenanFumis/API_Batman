c8b841ddb60d4d5349abc73fadbfebe42b73d805
RenanFumis <renan-oliveira19@hotmail.com>
2024-05-08 17:10:30 -0300

    Pasta de erros com arquivos separados para melhor manutenção das tratativas de erros na requisição

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
+import ErroBase from "../Erros/ErroBase.js";
+import ErroValidacao from "../Erros/ErroValidacao.js";
+import RequisicaoIncorreta from "../Erros/RequisicaoIncorreta.js";
 
 function manipuladorDeErros(erro, req, res, next){
   if (erro instanceof mongoose.Error.CastError){
-    res.status(400).send({ message: "Um ou mais parâmetros são inválidos."})
+    new RequisicaoIncorreta().enviarResposta(res);
   }else if(erro instanceof mongoose.Error.ValidationError){
-    const mensagensErro = Object.values(erro.errors).map((erro) => erro.message).join(", ");
     
-    res.status(400).send({message: `Os seguintes erros foram encontrados: ${mensagensErro}`});
+    new ErroValidacao(erro).enviarResposta(res);
   }else{
-    res.status(500).send({ message: `ERROR: Erro interno de Servidor.` });
+    new ErroBase().enviarResposta(res);
   }
 }
 
