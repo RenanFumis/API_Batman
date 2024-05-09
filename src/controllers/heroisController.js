8edaeb4296492702690b3508dc7dc0ef4a82b4cc
RenanFumis <renan-oliveira19@hotmail.com>
2024-05-08 22:44:10 -0300

    implementei filtros de busca por nome, profissão ou alter_ego, coloquei também um retuno de resposta caso um valor não seja encontrado na busca

--- src/controllers/heroisController.js
+++ src/controllers/heroisController.js
@@ -8,7 +8,7 @@ class HeroisController {
       const listarHerois = await heroi.find({});
       res.status(200).json(listarHerois);
     } catch (erro) {
-      next(erro)
+      next(erro);
     }
   }
 
@@ -16,10 +16,10 @@ class HeroisController {
     try {
       const id = req.params.id;
       const heroiEncontrado = await heroi.findById(id);
-      if(heroiEncontrado !== null){
+      if (heroiEncontrado !== null) {
         res.status(200).json(heroiEncontrado);
-      }else{
-        next(new NaoEncontrado("Heroi não encontrado"))
+      } else {
+        next(new NaoEncontrado("Heroi não encontrado"));
       }
     } catch (erro) {
       next(erro);
@@ -42,9 +42,9 @@ class HeroisController {
       const id = req.params.id;
       const heroiUpdate = await heroi.findByIdAndUpdate(id, req.body);
 
-      if(heroiUpdate !== null){
+      if (heroiUpdate !== null) {
         res.status(200).json({ message: "Heroi atualizado com sucesso" });
-      }else{
+      } else {
         next(new NaoEncontrado("Heroi não encontrado"));
       }
       res.status(200).json({ message: "Heroi atualizado com sucesso" });
@@ -58,37 +58,47 @@ class HeroisController {
       const id = req.params.id;
       const heroiEliminado = await heroi.findByIdAndDelete(id);
 
-      if(heroiEliminado !== null){
+      if (heroiEliminado !== null) {
         res.status(200).json({ message: "Heroi excluido com sucesso" });
-      }else{
+      } else {
         next(new NaoEncontrado("Heroi não encontrado"));
       }
-      
     } catch (erro) {
       next(erro);
     }
   }
 
-  static  listarHeroiPorFiltro = async (req, res, next) =>{
-    try{
+  static listarHeroiPorFiltro = async (req, res, next) => {
+    try {
       const { nome, alter_ego, profissao } = req.query;
+
+      //utilizando o regex do mongoose para fazer a busca mais organica
       const busca = {};
       //aqui fazemos a busca por nome e/ou alter_ego
       //para fazer a buca http://localhost:3000/herois/busca?nome=
-      if(nome) busca.nome = nome;
+      if (nome) busca.nome = { $regex: nome, $options: "i" };
       //para fazer a buca http://localhost:3000/herois/busca?alter_ego=
-      if(alter_ego) busca.alter_ego = alter_ego;
+      if (alter_ego) busca.alter_ego = { $regex: alter_ego, $options: "i" };
       //http://localhost:3000/herois/busca?profissao=
-      if(profissao) busca.profissao = profissao;
+      if (profissao) busca.profissao = { $regex: profissao, $options: "i" };
 
       const heroiResultado = await heroi.find(busca);
 
+      //caso não encontre nenhum heroi com o filtro especificado
+      if(heroiResultado.length === 0){
+
+        //verifica qual valor foi passado para o filtro
+        let valorPassado = nome ? "nome" : alter_ego ? "alter_ego" : profissao ? "profissao" : "";
+
+        //retorna uma mensagem com o valor não encontrado no filtro
+        return res.status(404).send({ message: `Nenhum herói registrado com o valor ${valorPassado ? valorPassado : "de busca"} '${req.query[valorPassado] ? req.query[valorPassado].toUpperCase() : ""}' especificado.` });
+      
+      }
       res.status(200).send(heroiResultado);
-    }catch(erro){
+    } catch (erro) {
       next(erro);
     }
-  }
-
+  };
 }
 
 export default HeroisController;
--- src/controllers/viloesController.js
+++ src/controllers/viloesController.js
@@ -3,88 +3,97 @@ import NaoEncontrado from "../Erros/NaoEncontrado.js";
 import vilao from "../models/Viloes.js";
 
 class ViloesController {
-
-  static async listarViloes(req, res, next){
-    try{
+  static async listarViloes(req, res, next) {
+    try {
       const listarViloes = await vilao.find({});
       res.status(200).json(listarViloes);
-    }catch (erro){
+    } catch (erro) {
       next(erro);
     }
   }
 
-  static async listarVilaoPorId(req, res, next){
-    try{
+  static async listarVilaoPorId(req, res, next) {
+    try {
       const id = req.params.id;
       const vilaoEncontrado = await vilao.findById(id);
-      if(vilaoEncontrado !== null){
+      if (vilaoEncontrado !== null) {
         res.status(200).json(vilaoEncontrado);
-      }else{
-        next(new NaoEncontrado("Vilão não encontrado"))
+      } else {
+        next(new NaoEncontrado("Vilão não encontrado"));
       }
-    }catch (erro){
+    } catch (erro) {
       next(erro);
     }
   }
 
-  static async adicionarVilao(req, res, next){
-    try{
+  static async adicionarVilao(req, res, next) {
+    try {
       const novoVilao = await vilao.create(req.body);
-      res.status(201).json({message: "Vilão adicionado com sucesso", vilao: novoVilao});
-    }catch (erro){
+      res
+        .status(201)
+        .json({ message: "Vilão adicionado com sucesso", vilao: novoVilao });
+    } catch (erro) {
       next(erro);
     }
   }
 
-  static async atualizarVilao(req, res, next){
-    try{
+  static async atualizarVilao(req, res, next) {
+    try {
       const id = req.params.id;
       const vilaoUpdate = await vilao.findByIdAndUpdate(id, req.body);
 
-      if(vilaoUpdate !== null){
-        res.status(200).json({message: "Vilão atualizado com sucesso"});
-      }else{
+      if (vilaoUpdate !== null) {
+        res.status(200).json({ message: "Vilão atualizado com sucesso" });
+      } else {
         next(new NaoEncontrado("Vilão não encontrado"));
       }
-      
-    }catch (erro){
+    } catch (erro) {
       next(erro);
     }
   }
 
-  static async excluirVilão(req, res, next){
-    try{
+  static async excluirVilão(req, res, next) {
+    try {
       const id = req.params.id;
       const vilaoEliminado = await vilao.findByIdAndDelete(id);
 
-      if(vilaoEliminado !== null){
-        res.status(200).json({message: "Vilão excluido com sucesso"});
-      }else{
+      if (vilaoEliminado !== null) {
+        res.status(200).json({ message: "Vilão excluido com sucesso" });
+      } else {
         next(new NaoEncontrado("Vilão não encontrado"));
       }
-      
-    }catch (erro){
+    } catch (erro) {
       next(erro);
     }
   }
-  static  listarViloesPorFiltro = async (req, res, next) =>{
-    try{
+  static listarViloesPorFiltro = async (req, res, next) => {
+    try {
       const { nome, alter_ego, profissao } = req.query;
       const busca = {};
       //para fazer a buca http://localhost:3000/viloes/busca?nome=
-      if(nome) busca.nome = nome;
+      if (nome) busca.nome = { $regex: nome, $options: "i" };
       //para fazer a buca http://localhost:3000/viloes/busca?alter_ego=
-      if(alter_ego) busca.alter_ego = alter_ego;
+      if (alter_ego) busca.alter_ego = { $regex: alter_ego, $options: "i" };
       //http://localhost:3000/viloes/busca?profissao=
-      if(profissao) busca.profissao = profissao;
+      if (profissao) busca.profissao = { $regex: profissao, $options: "i" };
 
       const viloesResultado = await vilao.find(busca);
 
+      if(viloesResultado.length === 0){
+
+        let valorPassado = nome ? "nome" : alter_ego ? "alter_ego" : profissao ? "profissao" : "";
+
+        return res.status(404).send({message:
+          `Nenhum vilão registrado com o valor ${valorPassado ? valorPassado : "de busca"} '${req.query[valorPassado] ? req.query[valorPassado].toUpperCase() : ""}' especificado.`
+        })
+      }
+
+
       res.status(200).send(viloesResultado);
-    }catch(erro){
+    } catch (erro) {
       next(erro);
     }
-  }
-};
+  };
+}
 
 export default ViloesController;
